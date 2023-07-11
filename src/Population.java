548892045b9c88e87d4ca9123f8a03acc067d0ae
akshay-seetharam <akshay.seetharam628@gmail.com>
2023-07-11 12:59:03 -0700

    fitness updates

--- src/Population.java
+++ src/Population.java
@@ -1,24 +1,25 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
+import java.util.Arrays;
 
 public class Population {
     private ArrayList<List<Boolean>> genome;
     private int genomeLength;
     private int populationSize;
     private double Sb;
-    private double activatedProportion;
+    private int startingMutations;
     private int generations;
     private int mutationsPerGeneration;
     private int transfersPerGeneration;
 
-    private int[] fitnesses;
+    private ArrayList<Integer> fitnesses;
 
-    public Population(int genomeLength, int populationSize, double Sb, double activatedProportion, int generations, int mutationsPerGeneration, int transfersPerGeneration) {
+    public Population(int genomeLength, int populationSize, double Sb, int startingMutations, int generations, int mutationsPerGeneration, int transfersPerGeneration) {
         this.genomeLength = genomeLength;
         this.populationSize = populationSize;
         this.Sb = Sb;
-        this.activatedProportion = activatedProportion;
+        this.startingMutations = startingMutations;
         this.generations = generations;
         this.mutationsPerGeneration = mutationsPerGeneration;
         this.transfersPerGeneration = transfersPerGeneration;
@@ -34,20 +35,30 @@ public class Population {
         for (int i = 0; i < populationSize; i++) {
             // make a list with activatedProportion of True, rest False
             ArrayList<Boolean> genome_i = new ArrayList<>();
-            for (int j = 0; j < (int) activatedProportion * genomeLength; j++) {
+            for (int j = 0; j < startingMutations; j++) {
                 genome_i.add(true);
             }
-            for (int k = genome_i.size(); k < genomeLength; k++) {
+            for (int k = startingMutations; k < genomeLength; k++) {
                 genome_i.add(false);
             }
             Collections.shuffle(genome_i);
             genome.add(genome_i);
         }
         System.out.println("Genome Initialized");
+        // System.out.println(genome);
     }
 
     private void initializeFitnesses() {
+        int trueCount = Arrays.deepToString(new ArrayList[]{genome}).replaceAll("[^t]", "").length();
+        fitnesses = new ArrayList<>();
+        fitnesses.add(trueCount);
         System.out.println("Fitnesses Initialized");
+        System.out.println(fitnesses);
+    }
+
+    private void updateFitnesses() {
+        int trueCount = Arrays.deepToString(new ArrayList[]{genome}).replaceAll("[^t]", "").length();
+        fitnesses.add(trueCount);
     }
 
     private void reproductiveUpdate() {
--- src/TransferAdaptation.java
+++ src/TransferAdaptation.java
@@ -5,7 +5,7 @@ public class TransferAdaptation {
     public static void main(String[] args) {
         System.out.println("Hello Transfer World");
 
-        Population population = new Population(5, 1000, 0.01, 0.05, 50, 5, 5);
+        Population population = new Population(5, 1000, 0.01, 2, 50, 5, 5);
 
     }
 }
